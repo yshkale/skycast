5e540417df2900688638ee10e4cc5d7d58d2b5dc
yshkale <therealyashkale@gmail.com>
2023-05-23 18:33:15 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,70 +1,35 @@
-# Getting Started with Create React App
+# Weather App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This is a weather application built using React.js and various APIs to provide users with real-time weather information based on their current location or a location of their choice. The app utilizes the OpenWeatherMap API, IP API, and Air Pollution API to fetch weather data, location information, and air pollution details, respectively.
 
-## Available Scripts
+## Features
 
-In the project directory, you can run:
+### Current Location Weather:
 
-### `npm start`
+Upon loading the app, the user is presented with the weather information of their current location, including temperature, weather conditions, wind speed, and humidity.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### Search by Location:
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Users can search for the weather of any location by entering the city name in the search input field. The app will fetch and display the weather data for the specified location.
 
-### `npm test`
+### Weather Details:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+The app provides detailed weather information, including the current temperature, minimum and maximum temperature, weather description, wind speed, humidity, and air pollution details.
 
-### `npm run build`
+## APIs Used
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+The following APIs were used to gather weather and location data:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+### OpenWeatherMap API:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+This API provides current weather data, including temperature, weather conditions, wind speed, and humidity.
 
-### `npm run eject`
+### IP API:
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+This API is used to fetch the user's current location based on their IP address.
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+### Air Pollution API:
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+The Air Pollution API is utilized to obtain air pollution details for a specific location.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+### If you would like to reach out to me, e-mail me at yashnkale@gmail.com
--- src/App.js
+++ src/App.js
@@ -1,24 +1,252 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import "./queries.css";
 
 function App() {
+  const [currentHour, setCurrentHour] = useState("");
+  const [currentMinute, setCurrentMinute] = useState("");
+
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [city, setCity] = useState("");
+
+  const [weatherData, setWeatherData] = useState({});
+
+  const [weatherIcon, setWeatherIcon] = useState("");
+
+  const [airData, setAirData] = useState({});
+  const [airQuality, setAirQuality] = useState("");
+
+  const [userSearch, setUserSearch] = useState("");
+
+  const handleSearch = function (e) {
+    setUserSearch(e.target.value);
+  };
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (userSearch !== "") {
+      try {
+        fetch(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${userSearch}&limit=1&appid=d64ce91bca2b8bd1d81cc2bebf950447`
+        )
+          .then((res) => res.json())
+          .then(([geoData]) => {
+            setCity(geoData.name);
+            setLatitude(geoData.lat.toFixed(2));
+            setLongitude(geoData.lon.toFixed(2));
+          })
+          .catch((err) => {
+            alert(
+              "Oops! couldn't find this location! Try searching for a different location."
+            );
+          });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (currentHour !== "" && currentHour > 19) {
+      setWeatherIcon("ClearNightV3.svg");
+    } else if (currentHour !== "" && currentHour < 19) {
+      setWeatherIcon("SunnyDayV3.svg");
+    }
+  }, [currentHour]);
+
+  useEffect(() => {
+    const currentDate = new Date();
+    setCurrentHour(currentDate.getHours());
+    setCurrentMinute(currentDate.getMinutes());
+  }, []);
+
+  useEffect(() => {
+    try {
+      const fetchLocation = async function () {
+        const res = await fetch("https://ipapi.co/json/");
+        const locationData = await res.json();
+        const { latitude, longitude, city } = locationData;
+
+        setLatitude(latitude.toFixed(2));
+        setLongitude(longitude.toFixed(2));
+        setCity(city);
+      };
+
+      fetchLocation().catch((err) => {
+        alert("Oops! Something went wrong! Try again later!");
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    try {
+      if (latitude !== "" && longitude !== "") {
+        fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=d64ce91bca2b8bd1d81cc2bebf950447`
+        )
+          .then((res) => res.json())
+          .then((data) => setWeatherData(data))
+          .catch((err) => {
+            alert("Oops! Something went wrong! Try again later!");
+          });
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, [latitude, longitude]);
+
+  useEffect(() => {
+    if (latitude !== "" && longitude !== "") {
+      try {
+        fetch(
+          `http://api.openweathermap.org/data/2.5/air_pollution?lat=${latitude}&lon=${longitude}&appid=d64ce91bca2b8bd1d81cc2bebf950447`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setAirData(data);
+
+            const aqi = data.list[0].main.aqi;
+            let airQ = "";
+
+            if (aqi === 1) {
+              airQ = "Good";
+            } else if (aqi === 2) {
+              airQ = "Fair";
+            } else if (aqi === 3) {
+              airQ = "Moderate";
+            } else if (aqi === 4) {
+              airQ = "Poor";
+            } else if (aqi === 5) {
+              airQ = "Very Poor";
+            } else {
+              airQ = "Unknown";
+            }
+
+            setAirQuality(airQ);
+          })
+          .catch((err) => {
+            alert("Oops! Something went wrong! Try again later!");
+          });
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }, [latitude, longitude]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <h1 className="logo">Sky Cast &mdash;</h1>
       </header>
-    </div>
+
+      <main>
+        <div className="main-container">
+          <div className="main-header">
+            <div>
+              <p className="current-weather">Current weather</p>
+              <p className="time">
+                {currentHour}:{currentMinute} {currentHour < 12 ? "AM" : "PM"}
+              </p>
+            </div>
+
+            <div>
+              <p className="location">{city}</p>
+            </div>
+          </div>
+
+          <div className="main-info">
+            {weatherIcon !== "" && (
+              <img
+                src={`https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v8/Condition_Card/${weatherIcon}`}
+              />
+            )}
+
+            <h1 className="degree">
+              {weatherData.main && Math.trunc(weatherData.main.temp - 273.15)}
+              &deg;C
+            </h1>
+            <div>
+              <p className="weather-type">
+                {weatherData.weather && weatherData.weather[0].main}
+              </p>
+              <p>
+                Feels like{" "}
+                {weatherData.main &&
+                  Math.trunc(weatherData.main.feels_like - 273.15)}
+                &deg;C
+              </p>
+            </div>
+          </div>
+          <p className="weather-description">
+            There will be mostly{" "}
+            {weatherData.weather && weatherData.weather[0].description}. The
+            high will be{" "}
+            {weatherData.main && Math.trunc(weatherData.main.temp_max - 273.15)}
+            °.
+          </p>
+
+          <div className="weather-info">
+            <div>
+              <p className="info-title">Air quality</p>
+              <p className="info-value">
+                {airQuality} ({airData.list && airData.list[0].main.aqi})
+              </p>
+            </div>
+            <div>
+              <p className="info-title">Wind</p>
+              <p className="info-value">
+                {weatherData.wind && weatherData.wind.speed.toFixed(1)} km/h
+              </p>
+            </div>
+            <div>
+              <p className="info-title">Humidity</p>
+              <p className="info-value">
+                {weatherData.main && weatherData.main.humidity}%
+              </p>
+            </div>
+            <div>
+              <p className="info-title">Visibility</p>
+              <p className="info-value">
+                {weatherData.visibility && weatherData.visibility / 1000} km
+              </p>
+            </div>
+            <div>
+              <p className="info-title">Pressure</p>
+              <p className="info-value">
+                {weatherData.main && weatherData.main.pressure} mb
+              </p>
+            </div>
+            <div>
+              <p className="info-title">Ground Level</p>
+              <p className="info-value">
+                {weatherData.main && weatherData.main.grnd_level} hpa
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <form className="search-container" onSubmit={handleSubmit}>
+          <input
+            className="search-input"
+            type="text"
+            name="search"
+            value={userSearch}
+            onChange={handleSearch}
+            placeholder="Search for a different location"
+          ></input>
+          <button className="search-btn" type="submit">
+            Search
+          </button>
+        </form>
+      </main>
+
+      <footer className="footer">
+        <p className="footer-info">Designed and built by Yash Kale.</p>
+      </footer>
+    </>
   );
 }
 
